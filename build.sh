096f6e9f15ffb509e062ed91a2d5bcd03a4d3a04
Martha Arenas <mararen@nube.unadmexico.mx>
2026-03-17 03:13:10 -0600

    Agregado superuser en Deploy

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'martha.arenas11@gmail.com', '@dm1n123#')" | python manage.py shell
